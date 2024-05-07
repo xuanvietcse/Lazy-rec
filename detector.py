18d8a6ae170793692b73955c183405c8fea07c48
xuvistudent <20522149@gm.uit.edu.vn>
2024-05-07 09:23:06 +0700

    Integrate realtime.py into CLI.

--- detector.py
+++ detector.py
@@ -13,12 +13,20 @@ TEXT_COLOR = "white"
 font = ImageFont.truetype(font="/home/xuanviet/.local/share/fonts/ttf/SanFranciscoProFonts/SF-Pro.ttf",size=30)
 
 parser = argparse.ArgumentParser(description="Recognize faces in an image")
-parser.add_argument("--train", action="store_true", help="Train on input data")
 parser.add_argument(
-    "--validate", action="store_true", help="Validate trained model"
+    "--train", 
+    action="store_true", 
+    help="Train on input data"
 )
 parser.add_argument(
-    "--test", action="store_true", help="Test the model with an unknown image"
+    "--validate", 
+    action="store_true", 
+    help="Validate trained model"
+)
+parser.add_argument(
+    "--test", 
+    action="store_true", 
+    help="Test the model with an unknown image"
 )
 parser.add_argument(
     "-m",
@@ -28,7 +36,14 @@ parser.add_argument(
     help="Which model to use for training: hog (CPU) or cnn (GPU)",
 )
 parser.add_argument(
-    "-f", action="store", help="Path to an image with an unknown face"
+    "-f", 
+    action="store", 
+    help="Path to an image with an unknown face"
+)
+parser.add_argument(
+    "--exec", 
+    action="store_true", 
+    help="Run in realtime"
 )
 args = parser.parse_args()
 
@@ -123,7 +138,81 @@ def validate(model: str="hog"):
                 image_location=str(filepath.absolute()), model=model
             )
 
-def realtime():
+def realtime(
+        model: str = "hog",
+        encodings_location: Path = DEFAULT_ENCODINGS_PATH
+) -> None:
+    # Get a reference to webcam #0 (the default one)
+    video_capture = cv2.VideoCapture(0)
+
+    # Initialize some variables
+    process_this_frame = True
+
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
+
+            # Conver the image from BGR color (which OpenCV using) to RGB color (which face_recognition using)
+            rgb_small_frame = small_frame[:,:,::-1]
+
+            #TODO: Fix compute_face_descriptor() on sample
+            code = cv2.COLOR_BGR2RGB
+            rgb_small_frame = cv2.cvtColor(rgb_small_frame, code)
+
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            # Load the encodings
+            with encodings_location.open(mode="rb") as f:
+                loaded_encodings = pickle.load(f)
+            
+            # 
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face
+                matches = face_recognition.compare_faces(loaded_encodings["encodings"], face_encoding)
+                name = "Unknown"
+
+                # Use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(loaded_encodings["encodings"], face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = loaded_encodings["names"][best_match_index]
+                face_names.append(name)
+        process_this_frame = not process_this_frame
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom -35), (right, bottom), (0, 0, 255), cv2.FILLED)
+
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name,(left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
 # Remove recognize_faces("unknown.jpg")
@@ -137,3 +226,5 @@ if __name__ == "__main__":
         validate(model=args.m)
     if args.test:
         recognize_faces(image_location=args.f, model=args.m)
+    if args.exec:
+        realtime(model=args.m, encodings_location=DEFAULT_ENCODINGS_PATH)
